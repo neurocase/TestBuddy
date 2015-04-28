24f9c9da6c8a15f7dd8196533039282af084d72d
neurocase <dazpetty@gmail.com>
2015-04-28 21:18:14 +1000

    updated delimiter is now :ANSWER:

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 TestBuddy
 simple python script to create practice tests for exams
 
-create a text file with the format question::answer, for example: what is 5+7::12
+create a text file with the format question:ANSWER:answer, for example: what is 5+7:ANSWER:12
 
 keep all questions and answers on same line then run in the console:
 
--- TestBuddy.py
+++ TestBuddy.py
@@ -1,37 +1,53 @@
 import os
 import random
 from colorama import Fore, Back, Style
+from string import whitespace
+import subprocess
+from optparse import OptionParser
 
 
 
+def formatStringForChecking(originalstring):
+    formattedstring = originalstring.replace('\"','\'')
+    formattedstring = formattedstring.replace(' ', '')
+    formattedstring = formattedstring.strip() 
+    return formattedstring
 
 def AnswerQuestions(content):
     random.shuffle(content)
+    debug = False
     wrong = 0
     totalquestions = 0
     wrongarr = list()
     for q in content:
-        print (q.partition('::')[0])
-        myanswer = raw_input(' ->  ')
-        theanswer = q.partition('::')[2]
-        nospacemyanswer = myanswer.replace(' ', '')
-        nospacemyanswer = myanswer.replace('\"','\'')
-        nospacetheanswer = theanswer.replace(' ', '')
-        nospacetheanswer = theanswer.replace('\"','\'')
-        #print(nospacemyanswer + "     " + nospacetheanswer)
-        if myanswer == 'finish':
-            FinishNow(wrong, totalquestions, wrongarr)
-        else:
-            totalquestions = totalquestions + 1
+        theanswer = q.partition(':ANSWER:')[2]
+        if theanswer != "(note)":
+            print (q.partition(':ANSWER:')[0])
+            myanswer = raw_input(' ->  ')
 
-        if nospacemyanswer.strip() == nospacetheanswer.strip():
-            print(Fore.YELLOW + "   " + theanswer.strip() + " is CORRECT" + Fore.RESET)
-            #if myanswer == theanswer.strip():
-            #   print("correct")
+
+            fmtmyanswer = formatStringForChecking(myanswer)
+            fmttheanswer = formatStringForChecking(theanswer)
+            #check the answer is not actually a command to finish or debug
+            if myanswer == 'finish':
+                FinishNow(wrong, totalquestions, wrongarr)
+            elif myanswer == 'debug':
+                debug = True
+            else:
+                # Assume is a valid attempt to answer the question
+                totalquestions = totalquestions + 1
+                if fmtmyanswer.strip() == fmttheanswer.strip():
+                    print(Fore.YELLOW + "   " + theanswer.strip() + " is CORRECT" + Fore.RESET)
+                else:
+                    print(Back.RED + "   WRONG, you said:" + myanswer + "   answer is:" + theanswer.strip() + "   " + Back.RESET)
+                    if debug == True:
+                        print(Back.BLUE + fmtmyanswer + ":ANSWER:" + fmttheanswer + Back.RESET)
+                    wrongarr.append(q)
+                    wrong = wrong + 1
         else:
-            print(Back.RED + "   WRONG, you said:" + myanswer + "   answer is:" + theanswer.strip() + "   " + Back.RESET)
-            wrongarr.append(q)
-            wrong = wrong + 1
+            print ("")
+            print (Back.WHITE + "  " + Back.BLUE + Fore.WHITE + "  " + q.partition(':ANSWER:')[0]  + "    " + Back.WHITE + "   " + Back.RESET + Fore.RESET) 
+            print ("")
     FinishNow(wrong, totalquestions, wrongarr)
 
 def FinishNow(wrong, totalquestions, wrongarr):
@@ -50,8 +66,17 @@ def FinishNow(wrong, totalquestions, wrongarr):
     exit()
 
 def Main():
+
+    parser = OptionParser()
+    parser.add_option("--dir", dest="currentdir", help="current directory to look for files")
+    (options, args) = parser.parse_args()
+    print options.currentdir
     print(Back.BLUE + "   " + ":: TestBuddy by Daryl Petty, dazpetty.com, Creative Commons, CC0, no rights reserved ::    " + Back.RESET)
     print(Fore.BLUE + "   " + ":: TestBuddy by DazPetty.com ::" + Fore.RESET)
+    pwd = subprocess.call("pwd")
+    print ("CURRENT DIRECTORY CONTAINS:")
+    flist = os.listdir(options.currentdir)
+    print flist
     tfname = raw_input("   practice test to open:")
     print("   " + ":: type 'finish' to end test ::")
     with open(tfname) as f:
